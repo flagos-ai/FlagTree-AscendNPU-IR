d906a39566ea9a0708472e38a3cf25505e18a405
Eugene Wu <wuzhenyu@xcoresigma.com>
2026-03-24 06:17:38 +0000

    [feat]:add no_alias attribute (from https://gitee.com/XcoreSigma/ascendnpu-ir/commit/3dbfebbcd690a1cc9ad83d023d957ebd885e141d)

--- bishengir/lib/Dialect/HIVM/Transforms/ConvertToHIVMOp.cpp
+++ bishengir/lib/Dialect/HIVM/Transforms/ConvertToHIVMOp.cpp
@@ -252,6 +252,8 @@ struct MemrefCopyOpLowering : public OpRewritePattern<memref::CopyOp> {
       if (implicitTransposeAttr.has_value()) {
         storeOp.setMayImplicitTransposeWithLastAxis(true);
       }
+
+      storeOp->setAttrs(copyOp->getAttrs());
       return success();
     }
 
